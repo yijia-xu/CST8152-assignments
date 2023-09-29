c21de3918dec00cf2024a8771f9bf7f5e87b2046
Yijia Xu <yijiaxu.yjx@gmail.com>
2023-09-29 09:27:06 -0400

    A12 Initialized

---
+++ A12/a12files/a1files/code/Compilers.h
@@ -0,0 +1,123 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Fall, 2023
+* Author: TO_DO
+* Professors: Paulo Sousa
+************************************************************
+=---------------------------------------=
+|  COMPILERS - ALGONQUIN COLLEGE (F23)  |
+=---------------------------------------=
+|              ....                     |
+|          ........::.::::::.           |
+|        .:........::.:^^^~~~:          |
+|        :^^::::::^^^::^!7??7~^.        |
+|       .:^~~^!77777~~7?YY?7??7^.       |
+|       :.^~!??!^::::^^~!?5PY??!~.      |
+|       ~!!7J~.:::^^^^~!!~~?G5J?~       |
+|       :^~?!~7?Y57^^?PP5YJ!J5Y?:       |
+|       .~!!.:^!7!:.:7JYYJ7~7Y7^        |
+|       .~77..    . .~^:^^^~7?:         |
+|       .^!^~:::.:^!7?~^~!77J:          |
+|        ^^!Y~^^^^~?YJ77??7JJ^          |
+|       .^7J?~^~~^~7??7??7JY?~:         |
+|        ::^^~^7?!^~~!7???J?J7~:.       |
+|         ^~~!.^7YPPPP5Y?7J7777~.       |
+|        ..:~..:^!JPP5YJ?!777!^.        |
+| .~?JJJJJJJJJJYYYYYPPPPPPPPPPPP5PPYY~  |
+|  :!Y5GGG.___ YYYYYY__._.PPGGGGGG5!.   |
+|   :!Y5G / __| ___ / _(_)__ _ PGP5.    |
+|    :~75 \__ \/ _ \  _| / _` | 5?.     |
+|     7~7 |___/\___/_| |_\__,_| Y5?.    |
+|    .^~!~.....................P5YY7.   |
+|   .:::::::::::::?JJJJYYYYYYYYYJJJJ7.  |
+|                                       |
+=---------------------------------------=
+*/
+
+/*
+************************************************************
+* File name: Compilers.h
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 – Compilers, Lab Section: [011, 012]
+* Assignment: A12, A22, A32.
+* Date: May 01 2023
+* Professor: Paulo Sousa
+* Purpose: This file defines the functions called by main function.
+* Function list: mainReader(), mainScanner(), mainParser().
+*************************************************************/
+
+#ifndef COMPILERS_H_
+#define COMPILERS_H_
+
+#define DEBUG 0
+
+/*
+ * ............................................................................
+ * ADVICE 1:
+ * Please check the "TODO" labels to develop your activity.
+ *
+ * ADVICE 2: This file must be updated according to each assignment
+ * (see the progression: reader > scanner > parser).
+ * ............................................................................
+ */
+
+/* TO_DO: Adjust your language (cannot be "Sofia") */
+
+/* Language name */
+#define STR_LANGNAME	"Sofia"
+
+/* Logical constants - adapt for your language */
+#define SOFIA_TRUE  1
+#define SOFIA_FALSE 0
+
+/*
+------------------------------------------------------------
+Data types definitions
+NOTE: Some types may not be directly used by your language,
+		but they can be necessary for conversions.
+------------------------------------------------------------
+*/
+
+
+/* TO_DO: Define your typedefs */
+typedef char			sofia_char;
+typedef char* sofia_string;
+typedef int				sofia_intg;
+typedef float			sofia_real;
+typedef void			sofia_void;
+
+typedef unsigned char	sofia_boln;
+typedef unsigned char	sofia_byte;
+
+typedef long			sofia_long;
+typedef double			sofia_doub;
+
+/*
+------------------------------------------------------------
+Programs:
+1: Reader - invokes MainReader code
+2: Scanner - invokes MainScanner code
+3: Parser - invokes MainParser code
+------------------------------------------------------------
+*/
+enum PROGRAMS {
+	PGM_READER	= 'r',
+	PGM_SCANNER = 's',
+	PGM_PARSER	= 'p'
+};
+
+/*
+------------------------------------------------------------
+Main functions signatures
+(Code will be updated during next assignments)
+------------------------------------------------------------
+*/
+sofia_intg mainReader(sofia_intg argc, sofia_string* argv);
+
+/* 
+TO_DO: Include later mainScaner (A22) and mainParser (A32)
+*/
+sofia_void printLogo();
+
+#endif
